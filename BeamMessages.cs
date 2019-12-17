a5ac30d43fd71ad01198e99ebb04491c42e88210
jim berry <jim@bigfattail.com>
2019-12-17 11:00:18 -0500

    Added enumerated message "types"

--- BeamMessages.cs
+++ BeamMessages.cs
@@ -1,26 +1,38 @@
+using GameModeMgr;
 namespace BeamBackend
 {
-    public class BeamMessages
+    public class BeamMessage
     {
-        public class GameCreatedMsg
-        {
-            public string gameId;
-            public GameCreatedMsg(string _gameId) => gameId = _gameId;
-        }
-        
-        public class GameJoinedMsg
-        {
-            public string gameId;
-            public string localId;
-            public GameJoinedMsg(string _gameId, string _localId) 
-            {
-                gameId = _gameId; 
-                localId = _localId;
-            }
-        }
-
-        public class RespawnMsg {}
-
-
+        public enum MsgType {
+            kGameCreated = 100,
+            kGameJoined = 101,
+            kPlayerJoined = 102,  
+        }            
+        public MsgType msgType;
+        public BeamMessage(MsgType t) => msgType = t;
     }
+
+
+    public class GameCreatedMsg : BeamMessage
+    {
+        public string gameId;
+        public GameCreatedMsg(string _gameId) : base(MsgType.kGameCreated) => gameId = _gameId;
+    }
+    
+    public class GameJoinedMsg : BeamMessage
+    {
+        public string gameId;
+        public string localId;
+        public GameJoinedMsg(string _gameId, string _localId) :  base(MsgType.kGameJoined)
+        {
+            gameId = _gameId; 
+            localId = _localId;
+        }
+    }
+
+    public class PlayerJoinedMsg : BeamMessage
+    {
+        public Player player;
+        public PlayerJoinedMsg(Player _p) : base(MsgType.kPlayerJoined) => player = _p;
+    }    
 }
