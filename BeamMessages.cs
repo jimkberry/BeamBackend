4d348345564d5fb4593b1c8ebae8d29b7294ef3d
jim berry <jim@bigfattail.com>
2019-12-18 12:28:14 -0500

    Add bike-related gamenet/backend messages

--- BeamMessages.cs
+++ BeamMessages.cs
@@ -1,4 +1,6 @@
 using GameModeMgr;
+using UnityEngine;
+
 namespace BeamBackend
 {
     public class BeamMessage
@@ -7,12 +9,19 @@ namespace BeamBackend
             kGameCreated = 100,
             kGameJoined = 101,
             kPeerJoined = 102,  
-            kPeerLeft = 103,           
+            kPeerLeft = 103,       
+            kBikeInfo = 104,
+            kBikeInfoReq = 105,
+            kBikeUpdate = 106,
+
         }            
         public MsgType msgType;
         public BeamMessage(MsgType t) => msgType = t;
     }
 
+    //
+    // Basic connection messages
+    //
 
     public class GameCreatedMsg : BeamMessage
     {
@@ -42,4 +51,58 @@ namespace BeamBackend
         public BeamPeer peer;
         public PeerLeftMsg(BeamPeer _p) : base(MsgType.kPeerLeft) => peer = _p;
     }      
+
+    //
+    // Bike-related messages
+    //
+    public class BikeInfoMsg : BeamMessage
+    {
+        public string bikeId; 
+        public string peerId;
+        public string name;
+        public Team team;
+        public int score;     
+        public int ctrlType;
+        public Vector2 position;
+        public Heading heading;     
+        public float speed;
+
+        public BikeInfoMsg(IBike ib) : base(MsgType.kBikeInfo) 
+        {
+            bikeId = ib.bikeId;
+            peerId = ib.peerId;
+            name = ib.name;
+            team = ib.team;
+            score = ib.score;
+            ctrlType = ib.ctrlType;
+            position = ib.position;
+            heading = ib.heading;
+            speed = ib.speed;
+        }
+    }
+
+    public class BikeInfoReq : BeamMessage
+    {
+        public string bikeId;
+        public BikeInfoReq(string _id) : base(MsgType.kBikeInfoReq) => bikeId = _id;        
+    }
+
+    public class BikeUpdateMsg : BeamMessage
+    {
+        public string bikeId; 
+        public int score;     
+        public Vector2 position;
+        public Heading heading;   
+        public float speed;  
+
+        public BikeUpdateMsg(IBike ib) : base(MsgType.kBikeUpdate) 
+        {
+            bikeId = ib.bikeId;
+            score = ib.score;
+            position = ib.position;
+            heading = ib.heading;
+            speed = ib.speed;
+        }
+    }
+
 }
