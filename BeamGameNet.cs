3bb0f43278f316dfa1db95d6b0c5be24de638e9f
jim berry <jim@bigfattail.com>
2020-01-21 15:16:53 -0500

    P2pNet restructure

--- BeamGameNet.cs
+++ BeamGameNet.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using UnityEngine;
 using GameNet;
+using P2pNet;
 
 namespace BeamBackend
 {
@@ -53,6 +54,36 @@ namespace BeamBackend
                 [BeamMessage.kPlaceHitReport] = (f,t,m) => this._HandlePlaceHitReport(f,t,m),                                
             };            
         }
+
+        protected override IP2pNet P2pNetFactory(string p2pConnectionString)
+        {
+            // P2pConnectionString is <p2p implmentation name>::<imp-dependent connection string>
+            // Names are: p2ploopback, p2predis
+
+            IP2pNet ip2p = null;
+            string[] parts = p2pConnectionString.Split(new string[]{"::"},StringSplitOptions.None); // Yikes! This is fugly.
+
+            switch(parts[0].ToLower())
+            {
+                case "p2predis":
+                    ip2p = new P2pRedis(this, parts[1]);
+                    break;
+                case "p2ploopback":
+                    ip2p = new P2pLoopback(this, null);
+                    break;          
+                // case "p2pactivemq":
+                //     p2p = new P2pActiveMq(this, parts[1]);
+                //     break;                               
+                default:
+                    throw( new Exception($"Invalid connection type: {parts[0]}"));
+            }
+
+            if (ip2p == null)
+                throw( new Exception("p2p Connect failed"));
+            
+            return ip2p;    
+        }
+
 
         public override void  CreateGame<GameCreationData>(GameCreationData data)
         {
