85c8755a6e8d6ada7d237ef790cf2db434308d73
jim berry <jim@bigfattail.com>
2020-03-30 14:28:03 -0400

    Don;t request unknown bibke data from yourself (does that really happen?)

--- BeamApianTrusty.cs
+++ BeamApianTrusty.cs
@@ -95,11 +95,12 @@ namespace BeamBackend
             if (ApianClock.IsIdle) // this is fugly
                 return;            
             BaseBike bb = gameData.GetBaseBike(msg.bikeId);
-            if (bb == null)
+            if (bb == null && srcId != ApianGroup.LocalP2pId)
             {
                 logger.Verbose($"OnPlaceHitObs() - unknown bike: {msg.bikeId}, source: {srcId}");
                 client.OnUnknownBike(msg.bikeId, msg.ownerPeer);
                 BeamGameNet.RequestBikeData(msg.bikeId, msg.ownerPeer);
+                return;
                 // TODO: think about what happens if we get the bike data before the vote is done.
                 // Should we: go ahead and count the incoming votes, but just not call OnPlaceHit() <- doing this now
                 //      while the bike isn't there
@@ -128,7 +129,7 @@ namespace BeamBackend
             if (ApianClock.IsIdle) // this is fugly
                 return;            
            BaseBike bb = gameData.GetBaseBike(msg.bikeId);
-            if (bb == null)
+            if (bb == null && srcId != ApianGroup.LocalP2pId)
             {
                 logger.Verbose($"OnPlaceClaimObs() - unknown bike: {msg.bikeId}, source: {srcId}");
                 client.OnUnknownBike(msg.bikeId, msg.ownerPeer);                
@@ -150,7 +151,7 @@ namespace BeamBackend
             if (ApianClock.IsIdle) // this is fugly
                 return;            
             BaseBike bb = gameData.GetBaseBike(msg.bikeId);
-            if (bb == null)
+            if (bb == null && srcId != ApianGroup.LocalP2pId)
             {
                 logger.Verbose($"OnBikeCommandReq() - unknown bike: {msg.bikeId}, source: {srcId}");
                 client.OnUnknownBike(msg.bikeId, msg.ownerPeer);               
@@ -166,7 +167,7 @@ namespace BeamBackend
             if (ApianClock.IsIdle) // this is fugly
                 return;            
             BaseBike bb = gameData.GetBaseBike(msg.bikeId);
-            if (bb == null)
+            if (bb == null  && srcId != ApianGroup.LocalP2pId)
             {
                 logger.Debug($"OnBikeTurnReq() - unknown bike: {msg.bikeId}, source: {srcId}");
                 client.OnUnknownBike(msg.bikeId, msg.ownerPeer);                
